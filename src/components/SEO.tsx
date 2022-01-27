cd3a3567ad57d97def0aa7aa802a56cc00633c52
AndyIsCool5463 <andyl5463@gmail.com>
2022-01-26 22:02:14 -0800

    add: favicons ☀

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -19,6 +19,31 @@ export default function SEO() {
         content={`https://meditate.app${router.pathname}`}
       />
       <meta property="og:type" content="website" />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#2b5797" />
+      <meta name="theme-color" content="#ffffff" />
     </Head>
   );
 }
